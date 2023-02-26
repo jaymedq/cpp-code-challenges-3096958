642bf225575c76c256e86385e38af803f5165849
Queiroz, Jayme <jaymedq@yahoo.com.br>
2023-02-26 12:53:28 -0300

    Horseplay solution

--- src/06/Challenge/ch6_horseplay.cpp
+++ src/06/Challenge/ch6_horseplay.cpp
@@ -7,6 +7,7 @@
 // Don't worry about other pieces on the chessboard.
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <string>
 
@@ -19,7 +20,28 @@ std::vector<std::string> knight_moves(std::string knight){
     std::vector<std::string> moves;
 
     // Write your code here
+    const int possibleMoves[8][2] = {{+1,+2}, {+2,+1}, {+2,-1}, {+1,-2}, {-1, -2}, {-2,-1}, {-2,+1}, {-1,+2}};
+    int column = 0;
+    int newColumn = 0;
+    int row = 0;
+    int newRow = 0;
 
+    std::transform(knight.begin(), knight.end(), knight.begin(), tolower);
+    column = static_cast<int>(knight[0]) - 'a';
+    row = static_cast<int>(knight[1]) - '1';
+
+    for(int i = 0; i < 8; i++)
+    {
+        newColumn = column + possibleMoves[i][0];
+        newRow = row + possibleMoves[i][1];
+        if((newColumn >= 1 && newColumn <=8)&&(newRow >= 1 && newRow <=8))
+        {
+            std::string new_loc = "  ";
+            new_loc[0] = newColumn + 'a';
+            new_loc[1] = newRow + '1';
+            moves.push_back(new_loc);
+        }
+    }
     return moves;
 }
 
